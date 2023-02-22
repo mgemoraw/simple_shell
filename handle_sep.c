83d1cb993040a6c8777107899261f52e350e8d64
amanabay <amanuelabay22@gmail.com>
2023-02-22 21:34:13 +0000

    Ignore everrything after #

---
+++ handle_sep.c
@@ -0,0 +1,41 @@
+#include "shell.h"
+#include "macros.h"
+
+/**
+ * separator - separates cmd recieved from stdin by ;
+ * @input: string gathered from stdin
+ *
+ * Return: parsed strings to be used
+ */
+
+char **separator(char *input)
+{
+	char **cmds;
+	char *cmd;
+	int i;
+	int buff_size = BUFSIZE;
+
+	if (input[0] == ' ' && input[_strlen(input)] == ' ')
+		exit(0);
+
+	if (input == NULL)
+		return (NULL);
+
+	cmds = malloc(sizeof(char *) * buff_size);
+	if (!cmds)
+	{
+		free(cmds);
+		perror("hsh");
+		return (NULL);
+	}
+
+	cmd = _strtok(input, ";&");
+	for (i = 0; cmd; i++)
+	{
+		cmds[i] = cmd;
+		cmd = _strtok(NULL, ";&");
+	}
+
+	cmds[i] = NULL;
+	return (cmds);
+}
