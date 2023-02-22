59331a8351c3a60d2041652bd88977b05362dd69
amanabay <amanuelabay22@gmail.com>
2023-02-22 15:38:53 +0000

    Own implementation of strtok

---
+++ strtok.c
@@ -0,0 +1,45 @@
+#include "shell.h"
+
+/**
+ * _strtok - separates strings with delimiters
+ * @line: It´s pointer to array we receive in getline.
+ * @delim: It´s characters we mark off string in parts.
+ *
+ * Return: A pointer to the created token
+*/
+
+char *_strtok(char *line, char *delim)
+{
+	int j;
+	static char *str;
+	char *tokens;
+
+	if (line != NULL)
+		str = line;
+	for (; *str != '\0'; str++)
+	{
+		for (j = 0; delim[j] != '\0'; j++)
+		{
+			if (*str == delim[j])
+			break;
+		}
+		if (delim[j] == '\0')
+			break;
+	}
+	tokens = str;
+	if (*tokens == '\0')
+		return (NULL);
+	for (; *str != '\0'; str++)
+	{
+		for (j = 0; delim[j] != '\0'; j++)
+		{
+			if (*str == delim[j])
+			{
+				*str = '\0';
+				str++;
+				return (tokens);
+			}
+		}
+	}
+	return (tokens);
+}
