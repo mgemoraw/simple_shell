88d92bf93c937a5c1b88f044c27c40a9aa2cb935
amanabay <amanuelabay22@gmail.com>
2023-02-22 15:13:55 +0000

    Adding header files

---
+++ shell.h
@@ -0,0 +1,36 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/*** STANDARD LIBRARIES ***/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <linux/limits.h>
+
+/* STRING HANDLER FUNCTIONS */
+
+char *_strncpy(char *dest, char *src, int n);
+int _strlen(char *s);
+int _putchar(char c);
+int _atoi(char *s);
+void _puts(char *str);
+int _strcmp(char *s1, char *s2);
+int _isalpha(int c);
+void rev_array(char *arr, int len);
+int int_len(int num);
+char *_itoa(unsigned int n);
+char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
+char *_strchr(char *s, char c);
+int _strncmp(const char *s1, const char *s2, size_t n);
+char *_strdup(char *str);
+
+#endif /*SHELL_H*/
